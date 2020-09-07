3c237bb9067c9a332a738d7083277bdcdde771b0
DESKTOP-G2RSQ2V\school <furafuratonbo@gmail.com>
2020-09-07 16:07:56 +0900

    stopwatch完成

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
 const timer = document.getElementById('timer');
 
+let timerTime;
+let endTime = 0;
+
 const startFunc = () => {
- const startTime = new Date();
+    if (start.classList.contains('active') === true ) {
+        const startTime = new Date();
+        const timerFunc = () => {
+         const now = new Date();
+         timerTime = now - startTime + endTime;
+         const ms = String(timerTime % 1000).padStart(3, '0');
+         const s = String(Math.floor(timerTime / 1000) % 60).padStart(2, '0');
+         const m = String(Math.floor(Math.floor(timerTime / 1000) / 60)).padStart(2, '0');
+         timer.textContent = `${m}:${s}:${ms}`;
+        }
+        start.classList.remove('active');
+        reset.classList.remove('active');
+        stop.classList.add('active');
+        return  setIntervalId = setInterval(timerFunc, 10);
+    }
+}
 
- const timerFunc = () => {
-  const now = new Date();
-  const timerTime = now - startTime;
-  const mm = String(timerTime % 1000).padStart(3, '0');
-  const s = String(Math.floor(timerTime / 1000) % 60).padStart(2, '0');
-  const m = String(Math.floor(Math.floor(timerTime / 1000) / 60)).padStart(2, '0');
-  timer.textContent = `${m}:${s}:${mm}`;
- }
+const stopFunc = () => {
+    if (stop.classList.contains('active') === true) {
+      clearInterval(setIntervalId);
+      endTime = timerTime;
+      stop.classList.remove('active');
+      start.classList.add('active');
+      reset.classList.add('active');
+    }
+}
 
- setInterval(timerFunc, 10);
+const resetFunc = () => {
+    if (reset.classList.contains('active') === true ) {
+      timer.textContent = '00:00:000';
+      endTime = 0;
+      reset.classList.remove('active');
+      start.classList.add('active');
+    }
 }
 
 start.addEventListener('click', startFunc);
+stop.addEventListener('click', stopFunc);
+reset.addEventListener('click', resetFunc);
